afe9bf428d194e6ed7250df733e026902e5a3dbc
PavinKM <pavinyagava@gmail.com>
2025-05-23 22:53:56 +0530

    json server on comment

--- react.jsx
+++ react.jsx
@@ -63,6 +63,50 @@ function CourseList(){
 }
 
 export default CourseList
+
+
 // npx json-server --watch data/dummyData.json --port 3000 --static ./data
 // npm create vite@latest
-//
+
+
+// data > db.json
+// {
+//     "posts" : [
+//         {
+//             "id": 101 ,
+//             "title": "My First Post" ,
+//             "content": "Hello, this is my first post!",
+//             "created_at": "2024-01-01"
+//         },
+//         {
+//             "id": 102 ,
+//             "title": "A Day in the Life" ,
+//             "content": "Today was a great day...",
+//             "created_at": "2024-01-01"
+//         },
+//         {
+//             "id": 103 ,
+//             "title": "Tech Trendz 2024" ,
+//             "content": "AI and Blockchain are shaping the future of technology",
+//             "created_at": "2024-01-05"
+//         },
+//         {
+//             "id": 104 ,
+//             "title": "Best Coding Practices" ,
+//             "content": "Writing clean and efficent code is essential for scalability",
+//             "created_at": "2024-01-10"
+//         },
+//         {
+//             "id": 105 ,
+//             "title": "The Future of Web Development" ,
+//             "content": "React, Svelte, and SolidJS are gaining traction amoung developers.",
+//             "created_at": "2024-02-15"
+//         },
+//         {
+//             "id": 106 ,
+//             "title": "Productivity Hacks" ,
+//             "content": "Using the Pomodoro technique has significantly improved",
+//             "created_at": "2024-02-20"
+//         }
+//     ]
+// }
